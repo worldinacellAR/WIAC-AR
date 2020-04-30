8c1c022414e087712956c74af00fecf5b87526c6
Abishek Narayanan <abishekn@usc.edu>
2020-04-29 22:37:22 -0700

    Updates

---
+++ precache-manifest.26b639d0e9815aec2b121f593df0bd52.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf53911e849b0ac45dbca1008e4ff0a",
+    "url": "/WIAC-AR/index.html"
+  },
+  {
+    "revision": "36aeb013a3ff7b4e5f1d",
+    "url": "/WIAC-AR/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "d85bc4d918d48c43836c",
+    "url": "/WIAC-AR/static/css/main.5b2b48c7.chunk.css"
+  },
+  {
+    "revision": "36aeb013a3ff7b4e5f1d",
+    "url": "/WIAC-AR/static/js/2.ca0cfdc5.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/WIAC-AR/static/js/2.ca0cfdc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d85bc4d918d48c43836c",
+    "url": "/WIAC-AR/static/js/main.1b9b89b3.chunk.js"
+  },
+  {
+    "revision": "7789744e38148dbe49ff",
+    "url": "/WIAC-AR/static/js/runtime-main.ff307e2f.js"
+  },
+  {
+    "revision": "c4bea1dc672b0e3686015e6b46f31749",
+    "url": "/WIAC-AR/static/media/WIAC00.c4bea1dc.jpg"
+  },
+  {
+    "revision": "320b9aff5287ae02a60982a27e260239",
+    "url": "/WIAC-AR/static/media/WIAC03.320b9aff.jpg"
+  },
+  {
+    "revision": "40e4f1f91b1f9f8b772d61009268c095",
+    "url": "/WIAC-AR/static/media/WIAC_Trailer.40e4f1f9.mp4"
+  },
+  {
+    "revision": "ed69c2fa5bffecbe7981828c70910721",
+    "url": "/WIAC-AR/static/media/angryimg.ed69c2fa.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WIAC-AR/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "00bcbd977c25d7850a7c5efdbc8ca9af",
+    "url": "/WIAC-AR/static/media/oneplayerdemo.00bcbd97.mp4"
+  },
+  {
+    "revision": "a1daf3ac5f0dbf902c8673e8f135079f",
+    "url": "/WIAC-AR/static/media/usc_games.a1daf3ac.png"
+  },
+  {
+    "revision": "53f87f1d04535d8f77d5308b51c429f7",
+    "url": "/WIAC-AR/static/media/v4.2.53f87f1d.png"
+  }
+]);
